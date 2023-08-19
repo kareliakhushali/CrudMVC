20da7de6923819f5cb16613ab5a9fb6c755cd37b
khushalikarlia15 <khushalikarelia15@gmail.com>
2023-08-19 11:17:07 +0530

    first commit

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudMVC.Models.ImageImplementation;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMVC.Data
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Laptop> Laptops { get; set;  }
+    }
+
+}
